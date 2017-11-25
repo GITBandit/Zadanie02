fec9548960c7472360c80120288adf26f1d95d42
Dominik Jakubowski <dominikjakubowski@gmail.com>
2017-11-25 22:04:41 +0100

    2 objects created basing on a default constructor. New constructor already prepared as a comment.

---
+++ src/Monitor.java
@@ -0,0 +1,20 @@
+class Monitor
+{
+
+    String manufacturer;
+    int size;
+    String proportions;
+    double price;
+
+    Monitor() {
+
+    }
+
+//    Monitor(String man, int si, String prop, float pri){
+//        manufacturer = man;
+//        size = si;
+//        proportions = prop;
+//        price = pri;
+//    }
+
+}
